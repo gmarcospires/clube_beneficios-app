a830b7fe7c26261d4f824a19162daa62cdc42f03
Marcos <gmarcospires@gmail.com>
2023-10-08 19:30:13 -0300

    Adicionado API - Descontos (Promoções) - Produtos - Produtos -> Descontos
    Ajuste na Factory
    Ajuste no seeder

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class UserController extends Controller
+{
+    private $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return $this->user->paginate(10);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // return $this->user->create([
+        //     'name' => $request->name,
+        //     'email' => $request->email,
+        //     'role' => 'user',
+        //     'password' => bcrypt($request->password),
+        //     'remember_token' => Str::random(10),
+        // ]);
+
+        return $this->user->create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return $this->user->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $user = $this->user->find($id);
+        $user->update($request->all());
+
+        return $user;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = $this->user->find($id);
+        $user->delete();
+
+        return $user;
+    }
+}
---
+++ app/Models/ProductsDiscount.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductsDiscount extends Model
+{
+    use HasFactory;
+
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
+    protected $fillable = [
+        'product_id',
+        'user_id',
+        'discount',
+        'valid_until',
+        'status'
+    ];
+     /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'valid_until' => 'datetime',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
