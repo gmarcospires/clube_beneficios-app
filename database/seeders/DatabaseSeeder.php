a406ed98e55784963e0b801af0b424a73eefd6cc
Marcos <gmarcospires@gmail.com>
2023-10-08 13:25:54 -0300

    Criando Factory para Produtos e DatabaseSeeder para mokar dados no banco

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,13 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'test@example.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin'
+        ]);
+
+        \App\Models\Product::factory(10)->create();
     }
 }
