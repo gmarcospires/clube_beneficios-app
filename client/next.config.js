d52f012832f8fa119cc77adb6b50ff7048b608c8
Marcos <gmarcospires@gmail.com>
2023-10-11 00:04:30 -0300

    Proxy Next.js para Laravel API

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8000/api/:path*',
+      },
+    ];
+  },
+  // async serverMiddleware() {
+  //   // Proxy API requests to Laravel
+  //   this.app.use(
+  //     '/api',
+  //     createProxyMiddleware({
+  //       target: 'http://localhost:8000',
+  //       changeOrigin: true,
+  //     })
+  //   );
+  // },
+};
