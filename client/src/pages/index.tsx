3da4a3f6dc614fbd4f1e69e7fc40347f269cf055
Marcos <gmarcospires@gmail.com>
2023-10-12 14:39:00 -0300

    Ajustes Navigation e página Incial

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,14 @@
 import { Container } from "@mui/material";
 import { useSession } from "next-auth/react";
+import Typography from "@mui/material/Typography";
 
 export default function Home() {
   const { data: session } = useSession();
   return (
     <Container className="flex flex-col  p-5">
-      <h1 className="self-center">Bem Vindo {session?.user?.name}</h1>
-      akjsbdasbjdbajks asdnlksakldnkas asd;kasnbdbasl c
+      <Typography variant="h5" className="self-center">
+        Bem vindo {session?.user?.name}
+      </Typography>
     </Container>
   );
 }
